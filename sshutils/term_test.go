c63f7e1edb82c3f12035ed968c14cb207ff527ee
Alfonso Cantos <alfonso.cantos@gmail.com>
2024-04-02 03:17:07 +0200

    adds term upload file test

--- sshutils/term_test.go
+++ sshutils/term_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestRunCommand(t *testing.T) {
+func TestTerm_Run(t *testing.T) {
 
 	if os.Getenv("INTEGRATION") == "" {
 		t.SkipNow()
@@ -22,4 +22,29 @@ func TestRunCommand(t *testing.T) {
 		biff.AssertEqual(out, "root\n")
 
 	})
+}
+
+func TestTerm_UploadFile(t *testing.T) {
+
+	if os.Getenv("INTEGRATION") == "" {
+		t.SkipNow()
+	}
+
+	f, _ := os.CreateTemp(".", "*")
+	defer func() {
+		os.Remove(f.Name())
+	}()
+	n, _ := f.WriteString("Hello world!")
+	biff.AssertEqual(n, 12)
+
+	environment(func(ctx context.Context) {
+
+		t, ok := ctx.Value("terminal").(*Term)
+		biff.AssertTrue(ok)
+
+		s, err := t.Upload(f.Name(), "file.txt")
+		biff.AssertNil(err)
+		biff.AssertEqual(s, int64(12))
+	})
+
 }
